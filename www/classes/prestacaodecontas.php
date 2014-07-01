4a610e2a3b02c28105ffed84418607844424b54e
TomeSilva <tom3lmsilva@gmail.com>
2014-07-01 20:08:48 +0100

    atualizaçao das classes
    pequena atualizaçao da classe prestacaodecontas

---
+++ www/classes/prestacaodecontas.php
@@ -0,0 +1,60 @@
+<?php
+
+class Prestacaodecontas {
+
+	private $resumo;
+	private $saldo;
+	private $porreceitas;
+	private $pordespesas;
+	private $porrubricas;
+	
+	function Prestacaodecontas(){
+		
+	}
+
+	
+	function get_resumo(){
+		return $resumo;
+	}
+
+	function set_resumo(){
+		
+	}
+
+	
+	function get_saldo(){
+		return $saldo;
+	}
+	
+	function set_saldo(){
+	
+	}
+	
+	
+	function get_porreceitas(){
+		return $sporreceitas;
+	}
+	
+	function set_porreceitas(){
+	
+	}
+	
+	
+	function get_pordespesas(){
+		return $spordespesas;
+	}
+	
+	function set_pordespesas(){
+	
+	}
+	
+	
+	function get_porrubricas(){
+		return $porrubricas;
+	}
+	
+	function set_porrubricas(){
+	
+	}
+}
+?>
